8bc3158b05c9e064728d0bd2f60205e2f7c051ca
haukurb <haukzi@gmail.com>
2025-10-17 19:54:42 +0000

    .

--- train.py
+++ train.py
@@ -156,11 +156,11 @@ class TruncatedLossTrainer(Trainer):
 
         # attention_mask = inputs["attention_mask"]
         # (B × T)
-        input_mask = input_ids.ne(self.tokenizer.pad_token_id)
+        input_mask = input_ids.ne(self.tokenizer.pad_token_id).char()
         # (B × T × 1) · (B × 1 × T) → (B × T × T)
         input_mask = input_mask.unsqueeze(-1) @ input_mask.unsqueeze(1)
         # (B × T × T)
-        attention_mask = input_mask.tril()
+        attention_mask = input_mask.tril().bool()
 
         bsz = input_ids.shape[0]
         seq_len = input_ids.shape[1]
