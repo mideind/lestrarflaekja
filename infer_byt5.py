04a876b22efaf729cd2ace9d0e07ac1058ed65fd
haukurb <haukzi@gmail.com>
2025-11-14 11:55:45 +0000

    .

--- infer_byt5.py
+++ infer_byt5.py
@@ -232,6 +232,7 @@ class SpanInfillingScorer:
             # middle
             target_ids = byte_ids_unshifted[loc_span_start:loc_span_end]
 
+            logger.debug(_chunk_idx)
             input_ids_w_masking = torch.cat([prefix, mask_seq, suffix], dim=1)
 
             logger.debug(input_ids_w_masking.shape)
